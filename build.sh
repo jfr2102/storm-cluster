786db7a65e4f5a8eebc0a57f9859ff1e2f054367
jfr <johannes@friedlein.eu>
2021-09-01 15:26:47 +0200

    Persist Grafana in volume - updated example topology.jar - minor changes in helperscripts

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 ./rm_container.sh
-sudo docker-compose up --build
+sudo docker-compose build zookeeper
+sudo docker-compose build nimbus
+sudo docker-compose build supervisor
+sudo docker-compose up --scale supervisor=2 
+#--build
 #--force-recreate
--- rm_container.sh
+++ rm_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-sudo docker rm my_ui
+# sudo docker rm my_ui
 sudo docker rm storm_graphite
-sudo docker rm my_zookeeper
-sudo docker rm my_nimbus
-sudo docker rm my_supervisor_1
-#storm_grafana
-
+# sudo docker rm my_zookeeper
+# sudo docker rm my_nimbus
+# sudo docker rm my_supervisor_1
+# sudo docker rm storm_grafana
+sudo docker rm $(sudo docker ps -aq -f name="(my|storm-cluster)_[_(a-z|1-9)*]*")
