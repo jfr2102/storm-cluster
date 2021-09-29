e9dc6730322aaa2cdfddb5008c05a80317976714
jfr2102 <johannes@friedlein.eu>
2021-09-29 21:51:12 +0200

    path to docker-stack.yml fix

--- buildscripts/push.sh
+++ buildscripts/push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f docker-stack.yml push
+docker-compose -f ../docker-stack.yml push
--- buildscripts/stackbuild.sh
+++ buildscripts/stackbuild.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-stack.yml build
+docker-compose -f ../docker-stack.yml build
