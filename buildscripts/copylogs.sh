ae5587ff18f809d7a497c288b793ba693ae223fc
jfr2102 <johannes@friedlein.eu>
2021-10-04 20:14:53 +0200

    - install wget storm image - copylogs.sh copies csv to apache2 folder

--- buildscripts/copylogs.sh
+++ buildscripts/copylogs.sh
@@ -5,3 +5,6 @@ mkdir ~/kafka-logs/$destination
 docker cp $container:/opt/kafka_2.13-2.7.1/produceroutput.csv kafka-logs/$destination/
 
 docker cp $container:/opt/kafka_2.13-2.7.1/resultsoutput.csv kafka-logs/$destination/
+# publish files via apache2:
+sudo cp kafka-logs/$destination/produceroutput.csv /var/www/html
+sudo cp kafka-logs/$destination/resultsoutput.csv /var/www/html
