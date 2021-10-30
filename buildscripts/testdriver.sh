a1f6bcf47ccb2a4aefe24d3ae50ea5c4f4024c79
jfr2102 <johannes@friedlein.eu>
2021-10-30 23:56:59 +0200

    cli arguments for Kafka generator

--- buildscripts/testdriver.sh
+++ buildscripts/testdriver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 container=`docker ps -qf name=kafka`
 docker exec -d -w /opt/kafka_2.13-2.7.1/ $container ./fetcher.sh
-docker exec -d -w /opt/kafka_2.13-2.7.1/ $container ./generator.sh
+docker exec -d -w /opt/kafka_2.13-2.7.1/ $container ./generator.sh $1 $2 $3 $4 $5
