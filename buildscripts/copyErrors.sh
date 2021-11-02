0e5592fa0cc4bb3de79801003f06bca31bd89676
jfr2102 <johannes@friedlein.eu>
2021-11-02 13:11:45 +0100

    destination folder with cli arguments

--- buildscripts/copyErrors.sh
+++ buildscripts/copyErrors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-destination=`date +"%Y-%m-%d_%H-%M-%S"`
+destination=`date +"%Y-%m-%d_%H-%M-%S"`_$1_$2
 container=`docker ps -qf name=nimbus`
 
 name1=`curl -s 'http://172.24.38.172:8080/api/v1/topology/summary' |  python3 -c "import sys, json; print(json.load(sys.stdin)['topologies'][0]['name'])"`
@@ -12,4 +12,4 @@ echo $name2 >> TopologyError.txt
 sudo docker exec -i $container storm get-errors $name2 >> TopologyError.txt
 
 sudo mkdir /storm-logs/$destination
-sudo cp TopologyError.txt /storm-logs/$destination 
+sudo cp TopologyError.txt /storm-logs/$destination
--- buildscripts/copylogs.sh
+++ buildscripts/copylogs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-destination=`date +"%Y-%m-%d_%H-%M-%S"`
+destination=`date +"%Y-%m-%d_%H-%M-%S"`_$1_$2
 container=`docker ps -qf name=kafka`
 mkdir ~/kafka-logs/$destination
 docker cp $container:/opt/kafka_2.13-2.7.1/produceroutput.csv kafka-logs/$destination/
