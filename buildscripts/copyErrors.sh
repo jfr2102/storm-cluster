b246c5ae31d5cdbdd7cc138b19bf30e668d13036
jfr2102 <johannes@friedlein.eu>
2021-11-10 21:50:07 +0100

    copy log scripts get timestamp as argument

--- buildscripts/copyErrors.sh
+++ buildscripts/copyErrors.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-destination=`date +"%Y-%m-%d_%H-%M-%S"`_$1_$2
+#destination=`date +"%Y-%m-%d_%H-%M-%S"`_$1_$2
+destination=$1
 container=`docker ps -qf name=nimbus`
 
 name1=`curl -s 'http://172.24.38.172:8080/api/v1/topology/summary' |  python3 -c "import sys, json; print(json.load(sys.stdin)['topologies'][0]['name'])"`
--- buildscripts/copylogs.sh
+++ buildscripts/copylogs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-destination=`date +"%Y-%m-%d_%H-%M-%S"`_$1_$2
+#destination=`date +"%Y-%m-%d_%H-%M-%S"`_$1_$2
+destination=$1
 container=`docker ps -qf name=kafka`
 mkdir ~/kafka-logs/$destination
 docker cp $container:/opt/kafka_2.13-2.7.1/produceroutput.csv kafka-logs/$destination/
