80ebdf6e1c0b0501e378c94b594604f5981ae83d
jfr2102 <johannes@friedlein.eu>
2021-12-07 20:48:51 +0100

    fix errorscript: add required spaces in: if[ condition ]

--- buildscripts/copyErrors.sh
+++ buildscripts/copyErrors.sh
@@ -5,7 +5,7 @@ container=`docker ps -qf name=nimbus`
 
 name1=`curl -s 'http://172.24.38.172:8080/api/v1/topology/summary' |  python3 -c "import sys, json; print(json.load(sys.stdin)['topologies'][0]['name'])"`
 
-if ["$2" == "CheckTopology"]
+if [ "$2" == "CheckTopology" ]
 then
     echo $name1 >> TopologyError.txt
 else
@@ -13,7 +13,7 @@ else
 fi
 sudo docker exec -i $container storm get-errors $name1 >> TopologyError.txt
 
-if ["$2" == "CheckTopology"]
+if [ "$2" == "CheckTopology" ]
 then
     sudo mkdir /storm-logs/$destination
     sudo cp TopologyError.txt /storm-logs/$destination
