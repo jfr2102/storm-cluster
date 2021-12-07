eeb68e8d7cce140bdcb8910ba08010647e8870f6
jfr2102 <johannes@friedlein.eu>
2021-12-07 18:38:07 +0100

    topology error fetch script executed seperately

--- buildscripts/copyErrors.sh
+++ buildscripts/copyErrors.sh
@@ -4,13 +4,17 @@ destination=$1
 container=`docker ps -qf name=nimbus`
 
 name1=`curl -s 'http://172.24.38.172:8080/api/v1/topology/summary' |  python3 -c "import sys, json; print(json.load(sys.stdin)['topologies'][0]['name'])"`
-name2=`curl -s 'http://172.24.38.172:8080/api/v1/topology/summary' |  python3 -c "import sys, json; print(json.load(sys.stdin)['topologies'][1]['name'])"`
 
-echo $name1 > TopologyError.txt
+if [$2 == CheckTopology]
+then
+    echo $name1 >> TopologyError.txt
+else
+    echo $name1 > TopologyError.txt
+fi
 sudo docker exec -i $container storm get-errors $name1 >> TopologyError.txt
 
-echo $name2 >> TopologyError.txt
-sudo docker exec -i $container storm get-errors $name2 >> TopologyError.txt
-
-sudo mkdir /storm-logs/$destination
-sudo cp TopologyError.txt /storm-logs/$destination
+if [$2 == CheckTopology]
+then
+    sudo mkdir /storm-logs/$destination
+    sudo cp TopologyError.txt /storm-logs/$destination
+fi
